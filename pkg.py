763a93ee87d8e81ad95695325d26c08eeeb1d66b
Barsef <117392025+Barsef@users.noreply.github.com>
2023-08-11 12:04:57 +0000

    fixed redundancy, added input checks

--- pkg.py
+++ pkg.py
@@ -7,13 +7,14 @@ class fileManager():
         self.pkg_list = self.getPkgs()
         self.repo_graph = self.getRepoGraph()
     def updatepkgs(self, pkg_list):
-        # Update the repo with the given list of packages
+        # Update pkgs.db with the given list of package names
         # pkg_list is a list of package names
         with open(self.pkg_path, 'w') as f:
-            for pkg in pkg_list:
+            for pkg in sorted(pkg_list):
                 f.write(pkg + '\n')
+        self.pkg_list = pkg_list
     def getPkgs(self):
-        # Return a list of package names
+        # Return a list of package names in pkgs.db
         with open(self.pkg_path, 'r') as f:
             return [line.strip() for line in f.readlines()]
     def getRepoGraph(self):
@@ -40,25 +41,23 @@ class fileManager():
         # Return a set of dependencies
         graph = self.repo_graph
         dependencies = set()
-        for package in graph:
-            if package == pkg_name:
-                dependencies.update(graph[package])
-                for dep in graph[package]:
-                    dependencies.update(self.getDependencies(dep))
+        if pkg_name in graph:
+            dependencies.update(graph[pkg_name])
+            for dep in graph[pkg_name]:
+                dependencies.update(self.getDependencies(dep))
         return dependencies
     
     def install(self,pkg_name):
         # Install the given package and dependencies
         dependecies = self.getDependencies(pkg_name)
-        print("installing", pkg_name, dependecies)
-        self.pkg_list.append(pkg_name)
-        self.pkg_list.extend(dependecies)
+        dependecies.update([pkg_name])
+        dependecies.update(self.pkg_list)
+        self.pkg_list= list(dependecies)
         self.updatepkgs(self.pkg_list)
 
     def uninstall(self,pkg_name):
         # Uninstall the given package and dependents
         dependents = self.getDependents(pkg_name)
-        print("uninstalling", pkg_name, dependents)
         self.pkg_list.remove(pkg_name)
         for dep in dependents:
             self.pkg_list.remove(dep)
@@ -66,7 +65,6 @@ class fileManager():
 
 def list_packages(pkgs_path="pkgs.db"):
     # List all installed packages
-    print("Installed packages:")
     with open(pkgs_path, 'r') as f:
         for line in f:
             print(line.strip())
@@ -74,18 +72,19 @@ def list_packages(pkgs_path="pkgs.db"):
 
 if __name__ == "__main__":
     command = sys.argv[1]
-    fm = fileManager()
     if command not in ["install", "uninstall", "list"]:
         print("Invalid command")
         sys.exit(1)
     if command == "install":
-        print("install started")
+        fm = fileManager()
         pkg_name = sys.argv[2]
         fm.install(pkg_name)
     if command == "uninstall":
-        print("uninstall started")
+        fm = fileManager()
         pkg_name = sys.argv[2]
+        if pkg_name not in fm.pkg_list:
+            print("Package not installed")
+            sys.exit(1)
         fm.uninstall(pkg_name)
     if command == "list":
-        print("list started")
         list_packages()
